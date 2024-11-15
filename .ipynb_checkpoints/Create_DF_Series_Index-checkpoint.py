9d36803a5e36fff00ee2d1542d872522e80a5d2b
samsheer <samsheer.tc@gmail.com>
2024-11-14 23:22:42 -0500

    Commit on 11/14

--- .ipynb_checkpoints/Create_DF_Series_Index-checkpoint.py
+++ .ipynb_checkpoints/Create_DF_Series_Index-checkpoint.py
@@ -1,20 +1,28 @@
 DataFrame
 ---------
-pd.DataFrame(dictionary)
-d1=[[1,2,3],[11,12,13],[21,22,23]]
-d2=[(1,2,3),(11,12,13),(21,22,23)]
-d3=([1,2,3],[11,12,13],[21,22,23])
-d4=((1,2,3),(11,12,13),(21,22,23))
 idx=[1,2,3]
-col=['A', 'B', 'C']
+col=['Col1', 'Col2', 'Col3']
+
+d1 = { 
+    'Col1' : ['key1_v1', 'key1_v2','key1_v3'],
+    'Col2' : ['key2_v1', 'key2_v2','key2_v3'],
+    'Col3' : ['key3_v1', 'key3_v2','key3_v3'],
+}
+
+d2=[[1,2,3],[11,12,13],[21,22,23]]
+d3=[(1,2,3),(11,12,13),(21,22,23)]
+d4=([1,2,3],[11,12,13],[21,22,23])
+d5=((1,2,3),(11,12,13),(21,22,23))
+
 pd.DataFrame(data=d1,index=idx,columns=col)
+
 
 Series
 ------
 idx=['a','b','c']
 d1 = [1,2,3]
 d2 = (1,2,3)
-pd.Series(data=d1,index=idx)
+pd.Series(data=d1,index=idx,name='Items',dtype='float64')
 
 
 MultiIndex
@@ -23,7 +31,12 @@ d1=[[1,2,3],[11,12,13],[21,22,23]]
 d2=[(1,2,3),(11,12,13),(21,22,23)] ****
 d3=([1,2,3],[11,12,13],[21,22,23])
 d4=((1,2,3),(11,12,13),(21,22,23))
-pd.MultiIndex.from_arrays(d1)
+pd.MultiIndex.from_arrays(d1,names=['level1', 'level2','level3'])
+
+MultiIndex([(1, 11, 21),
+            (2, 12, 22),
+            (3, 13, 23)],
+           names=['level1', 'level2', 'level3'])
 
 eg:
 MultiIndex([('mammal',   'cat'),
@@ -34,7 +47,7 @@ MultiIndex([('mammal',   'cat'),
 
 
 MultiIndex.from_product
------------------------
+----------------------
 data = [[0, 1, 2], ['green', 'purple']]
 data = [(0, 1, 2), ('green', 'purple')]**
 data = ([0, 1, 2], ['green', 'purple'])
